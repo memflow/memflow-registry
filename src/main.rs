a666579bb4f696e09b00eee17868b17b24e4c5fd
ko1N <ko1N1337@gmail.com>
2024-03-07 20:25:27 +0100

    Added basic parsing of Descriptor from PE file

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use axum::{routing::get, Router};
+
+mod storage;
+
+#[tokio::main]
+async fn main() {
+    // test code
+    let store = storage::Storage::new();
+    //
+
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let addr = std::env::var("MEMFLOW_ADDR").unwrap_or_else(|_| "0.0.0.0:3000".into());
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
---
+++ src/storage.rs
@@ -0,0 +1,84 @@
+use std::ffi::CStr;
+
+use axum::extract::ConnectInfo;
+use goblin::{
+    container::Ctx,
+    elf::{dynamic, Dynamic, Elf, ProgramHeader, RelocSection, Symtab},
+    mach::{exports::ExportInfo as MachExportInfo, Mach, MachO},
+    pe::{options::ParseOptions, PE},
+    strtab::Strtab,
+    Object,
+};
+use memflow::cglue::CSliceRef;
+use memflow::plugins::ConnectorDescriptor;
+
+/// Metadata attached to each file
+pub struct FileMetadata {
+    pub plugin: String,
+    // TODO: plugin type
+    pub tag: String,
+    // TODO: memflow version / abi version
+    // TODO: plugin version
+}
+
+///
+pub struct Storage {}
+
+impl Storage {
+    pub fn new() -> Self {
+        // TODO: re-index all files
+        let paths = std::fs::read_dir("./.storage").unwrap();
+        for path in paths {
+            let bytes = std::fs::read(path.unwrap().path()).unwrap();
+            let obj = Object::parse(&bytes).unwrap();
+            match obj {
+                Object::PE(pe) => {
+                    for export in pe.exports.iter() {
+                        if let Some(name) = export.name {
+                            if name.starts_with("MEMFLOW_CONNECTOR_")
+                                || name.starts_with("MEMFLOW_OS_")
+                            {
+                                let offset = export.offset.unwrap();
+
+                                // TODO: get rid of this unsafeness
+                                let descriptor_ptr: *const ConnectorDescriptor =
+                                    bytes.as_ptr().wrapping_add(offset) as *const _; // unsafe { std::mem::transmute::<_, ConnectorDescriptor>() };
+                                let descriptor =
+                                    unsafe { &*descriptor_ptr } as &ConnectorDescriptor;
+
+                                println!("plugin_version: {}", descriptor.plugin_version);
+                                println!(
+                                    "name: {}",
+                                    pe_read_sliceref(&bytes, &pe, &descriptor.name)
+                                );
+                                println!(
+                                    "version: {}",
+                                    pe_read_sliceref(&bytes, &pe, &descriptor.version)
+                                );
+                                println!(
+                                    "description: {}",
+                                    pe_read_sliceref(&bytes, &pe, &descriptor.description)
+                                );
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        Self {}
+    }
+}
+
+fn pe_read_sliceref(bytes: &[u8], pe: &PE, str: &CSliceRef<u8>) -> String {
+    let offset_va = str.as_ptr() as usize - pe.image_base;
+    let offset =
+        goblin::pe::utils::find_offset(offset_va, &pe.sections, 8, &ParseOptions::default())
+            .unwrap();
+
+    let mut buffer = vec![0u8; str.len()];
+    buffer.copy_from_slice(&bytes[offset..offset + str.len()]);
+
+    std::str::from_utf8(&buffer[..]).unwrap().to_owned()
+}
