c55a7d1104be76f0f872a20a3a41baa89338e1e3
ko1N <ko1N1337@gmail.com>
2024-03-09 15:14:18 +0100

    Improved parsing of descriptor

--- src/storage.rs
+++ src/storage.rs
@@ -1,4 +1,4 @@
-use std::ffi::CStr;
+use std::{ffi::CStr, marker::PhantomData, path::Path};
 
 use axum::extract::ConnectInfo;
 use goblin::{
@@ -9,8 +9,8 @@ use goblin::{
     strtab::Strtab,
     Object,
 };
-use memflow::cglue::CSliceRef;
-use memflow::plugins::ConnectorDescriptor;
+use memflow::plugins::{ConnectorDescriptor, LoadableConnector, PluginDescriptor};
+use memflow::{cglue::CSliceRef, plugins::MEMFLOW_PLUGIN_VERSION};
 
 /// Metadata attached to each file
 pub struct FileMetadata {
@@ -24,59 +24,145 @@ pub struct FileMetadata {
 ///
 pub struct Storage {}
 
+pub struct DescriptorFile<'a> {
+    bytes: &'a [u8],
+    object: Object<'a>,
+}
+
+pub struct Descriptor<'a> {
+    bytes: &'a [u8],
+    object: &'a Object<'a>,
+    plugin_descriptor: ConnectorDescriptor,
+}
+
+impl<'a> DescriptorFile<'a> {
+    pub fn new(bytes: &'a [u8]) -> Self {
+        let object = Object::parse(&bytes[..]).unwrap();
+        Self { bytes, object }
+    }
+
+    pub fn descriptors(&self) -> Vec<Descriptor> {
+        let mut ret = vec![];
+        match &self.object {
+            Object::PE(pe) => {
+                for export in pe.exports.iter() {
+                    if let Some(name) = export.name {
+                        if name.starts_with("MEMFLOW_CONNECTOR_") || name.starts_with("MEMFLOW_OS_")
+                        {
+                            let offset = export.offset.unwrap();
+
+                            use memflow::dataview::DataView;
+                            let data_view = DataView::from(self.bytes);
+                            let descriptor = data_view.read::<ConnectorDescriptor>(offset);
+
+                            ret.push(Descriptor {
+                                bytes: self.bytes,
+                                object: &self.object,
+                                plugin_descriptor: descriptor,
+                            });
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        ret
+    }
+}
+
+impl<'a> Descriptor<'a> {
+    #[inline]
+    pub fn plugin_version(&self) -> i32 {
+        self.plugin_descriptor.plugin_version
+    }
+
+    #[inline]
+    pub fn is_compatible(&self) -> bool {
+        self.plugin_version() == MEMFLOW_PLUGIN_VERSION
+    }
+
+    #[inline]
+    pub fn name(&self) -> String {
+        self.read_sliceref(&self.plugin_descriptor.name)
+    }
+
+    #[inline]
+    pub fn version(&self) -> String {
+        let obj = Object::parse(self.bytes).unwrap();
+        let descriptors = self.descriptor(&obj);
+        let descriptor = descriptors.first().unwrap();
+        self.read_sliceref(&descriptor.version)
+    }
+
+    #[inline]
+    pub fn description(&self) -> String {
+        let obj = Object::parse(self.bytes).unwrap();
+        let descriptors = self.descriptor(&obj);
+        let descriptor = descriptors.first().unwrap();
+        self.read_sliceref(&descriptor.description)
+    }
+
+    fn descriptor(&self, obj: &goblin::Object) -> Vec<ConnectorDescriptor> {
+        match obj {
+            Object::PE(pe) => {
+                for export in pe.exports.iter() {
+                    if let Some(name) = export.name {
+                        if name.starts_with("MEMFLOW_") {
+                            let offset = export.offset.unwrap();
+
+                            use memflow::dataview::DataView;
+                            let data_view = DataView::from(self.bytes);
+                            let descriptor = data_view.read::<ConnectorDescriptor>(offset);
+                            return vec![descriptor];
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        vec![]
+    }
+
+    fn read_sliceref(&self, str: &CSliceRef<u8>) -> String {
+        match self.object {
+            Object::PE(pe) => {
+                let offset_va = str.as_ptr() as usize - pe.image_base;
+                let offset = goblin::pe::utils::find_offset(
+                    offset_va,
+                    &pe.sections,
+                    8,
+                    &ParseOptions::default(),
+                )
+                .unwrap();
+
+                let mut buffer = vec![0u8; str.len()];
+                buffer.copy_from_slice(&self.bytes[offset..offset + str.len()]);
+
+                std::str::from_utf8(&buffer[..]).unwrap().to_owned()
+            }
+            _ => "".to_owned(),
+        }
+    }
+}
+
 impl Storage {
     pub fn new() -> Self {
         // TODO: re-index all files
         let paths = std::fs::read_dir("./.storage").unwrap();
-        for path in paths {
-            let bytes = std::fs::read(path.unwrap().path()).unwrap();
-            let obj = Object::parse(&bytes).unwrap();
-            match obj {
-                Object::PE(pe) => {
-                    for export in pe.exports.iter() {
-                        if let Some(name) = export.name {
-                            if name.starts_with("MEMFLOW_CONNECTOR_")
-                                || name.starts_with("MEMFLOW_OS_")
-                            {
-                                let offset = export.offset.unwrap();
-
-                                use memflow::dataview::DataView;
-                                let data_view = DataView::from(&bytes[..]);
-                                let descriptor = data_view.read::<ConnectorDescriptor>(offset);
-
-                                println!("plugin_version: {}", descriptor.plugin_version);
-                                println!(
-                                    "name: {}",
-                                    pe_read_sliceref(&bytes, &pe, &descriptor.name)
-                                );
-                                println!(
-                                    "version: {}",
-                                    pe_read_sliceref(&bytes, &pe, &descriptor.version)
-                                );
-                                println!(
-                                    "description: {}",
-                                    pe_read_sliceref(&bytes, &pe, &descriptor.description)
-                                );
-                            }
-                        }
-                    }
-                }
-                _ => {}
+        for path in paths.filter_map(|p| p.ok()) {
+            let bytes = std::fs::read(path.path()).unwrap();
+            let file = DescriptorFile::new(&bytes[..]);
+            let descriptors = file.descriptors();
+            for descriptor in descriptors.iter() {
+                println!("plugin_version: {}", descriptor.plugin_version());
+                println!("name: {}", descriptor.name());
+                println!("version: {}", descriptor.version());
+                println!("description: {}", descriptor.description());
             }
         }
 
         Self {}
     }
-}
-
-fn pe_read_sliceref(bytes: &[u8], pe: &PE, str: &CSliceRef<u8>) -> String {
-    let offset_va = str.as_ptr() as usize - pe.image_base;
-    let offset =
-        goblin::pe::utils::find_offset(offset_va, &pe.sections, 8, &ParseOptions::default())
-            .unwrap();
-
-    let mut buffer = vec![0u8; str.len()];
-    buffer.copy_from_slice(&bytes[offset..offset + str.len()]);
-
-    std::str::from_utf8(&buffer[..]).unwrap().to_owned()
 }
