a432f6abd9cdb3296a9faead72cebfecad7c68f5
ko1N <ko1N1337@gmail.com>
2024-03-26 14:44:01 +0100

    Moved default verifying key to memflow-registry-shared

--- memflow-registry-shared/src/lib.rs
+++ memflow-registry-shared/src/lib.rs
@@ -1,4 +1,8 @@
 pub const MEMFLOW_DEFAULT_REGISTRY: &str = "https://registry.memflow.io";
+pub const MEMFLOW_DEFAULT_REGISTRY_VERIFYING_KEY: &str = "-----BEGIN PUBLIC KEY-----
+MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEwFb+pnLXeLpW3n1utc0a18PfnV1fMaxq
+wt3vkyIaLMfMcmIUISq9c51CsNLKPYxzOGS7PW3Nyd0NQWjCvR74mQ==
+-----END PUBLIC KEY-----";
 
 pub mod error;
 pub mod pki;
